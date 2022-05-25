3460f9d29b5d8b7f87c9835ac4b6993f099e992b
unknown <ajfoucaultmo@mail.usf.edu>
2022-05-24 20:29:53 -0700

    25. Add redirect files for Netlify and final touch ups

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -13,20 +13,20 @@ import {
   getFirestore,
   doc,
   getDoc,
-  getDocs,
   setDoc,
   collection,
   writeBatch,
   query,
+  getDocs,
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBPr2JoQOex7lYpRcgWYsfDwUHSq94cORs',
-  authDomain: 'crwn-clothing-db-d1463.firebaseapp.com',
-  projectId: 'crwn-clothing-db-d1463',
-  storageBucket: 'crwn-clothing-db-d1463.appspot.com',
-  messagingSenderId: '1091799575742',
-  appId: '1:1091799575742:web:fd4decf4f8341313a7c2ef',
+  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+  projectId: 'crwn-clothing-db-98d4d',
+  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+  messagingSenderId: '626766232035',
+  appId: '1:626766232035:web:506621582dab103a4d08d6',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -47,10 +47,11 @@ export const db = getFirestore();
 
 export const addCollectionAndDocuments = async (
   collectionKey,
-  objectsToAdd
+  objectsToAdd,
+  field
 ) => {
-  const batch = writeBatch(db);
   const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
 
   objectsToAdd.forEach((object) => {
     const docRef = doc(collectionRef, object.title.toLowerCase());
